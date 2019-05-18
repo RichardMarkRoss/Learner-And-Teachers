9df2f03da6d57086e7275818708058f3e5965e72
RichardMarkRoss <cptrichkidprice@gmail.com>
2019-05-18 12:46:47 +0200

    added commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import sun.tools.jstat.Scale;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);_
+
+        String input;
+
+        while(true) {
+            input = userInput.nextLine();
+            
+        }
+    }
+}
